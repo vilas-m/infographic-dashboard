84538d54b5447cb76e8475666194ea4331f77366
vilas-m <vilas@humancloud.co.in>
2021-04-23 12:42:27 +0530

    Added donut chart

---
+++ src/Charts/DonutChart.js
@@ -0,0 +1,68 @@
+import React, { useEffect } from "react";
+import * as d3 from "d3";
+
+const DonutChart = ({color, value, id}) => {
+  useEffect(() => {
+    draw();
+  });
+
+  let draw = () => {
+    d3.select("#donutChart" + id).select("svg").remove();
+
+    let margin = 10;
+    let width = 70;
+    let height = 70;
+
+    let radius = Math.min(width, height) / 2 - margin;
+
+    let svg = d3
+      .select("#donutChart" + id)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("g")
+      .attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+    svg
+      .append("path")
+      .attr(
+        "d",
+        d3
+          .arc()
+          .startAngle(0)
+          .endAngle(2 * Math.PI)
+          .innerRadius(radius * 0.8)
+          .outerRadius(radius)
+          .cornerRadius(10)
+      )
+      .attr("fill", "#1d2c4e")
+      .attr("opacity", 0.7);
+
+    svg
+      .append("path")
+      .attr(
+        "d",
+        d3
+          .arc()
+          .startAngle(0)
+          .endAngle(Number(value)/360 * Math.PI)
+          .innerRadius(radius * 0.8)
+          .outerRadius(radius)
+          .cornerRadius(10)
+      )
+      .attr("fill", color);
+
+      svg
+      .append("text")
+      .attr("x", 0)
+      .attr("y", 5)
+      .text(value)
+      .style("font-size", 17)
+      .attr("fill", '#fff')
+      .attr("text-anchor", "middle")
+  };
+
+  return <div id={"donutChart" + id}></div>;
+};
+
+export default DonutChart;
